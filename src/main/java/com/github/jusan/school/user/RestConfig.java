175ebdd69a9a6ef2eb0592e29ab347900940777d
makhambet.torezhan <makhambet.torezhan@nu.edu.kz>
2022-06-02 19:22:13 +0600

    add user entry

--- src/main/java/com/github/jusan/school/user/RestConfig.java
+++ src/main/java/com/github/jusan/school/user/RestConfig.java
@@ -2,6 +2,7 @@ package com.github.jusan.school.user;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,5 @@
 INSERT INTO student (first_name, last_name, password, email) values
  ('a', 'b', '$2a$16$28myhXGaex5e106VEskTxe3y.ZeaD8E66NUxoV5iO91iZABM0O9SK', 'w@a.com'),
- ('s', 's', '$2a$16$28myhXGaex5e106VEskTxe3y.ZeaD8E66NUxoV5iO91iZABM0O9SK',  'w2@a.com');
+ ('s', 's', '$2a$16$28myhXGaex5e106VEskTxe3y.ZeaD8E66NUxoV5iO91iZABM0O9SK',  'w2@a.com'),
+ ('s1', 's2', '$2a$16$28myhXGaex5e106VEskTxe3y.ZeaD8E66NUxoV5iO91iZABM0O9SK',  'mt');
 
